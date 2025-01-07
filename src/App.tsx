46ae40951cdc60d9258dc7d7f358b2743525f30f
selimdoyranli <selimdoyranli@gmail.com>
2025-01-07 18:34:05 +0300

    refactor: ci/cd tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
-import { Router } from '@/Router';
 
+import { MantineProvider } from '@mantine/core';
 import useTheme from '@/composables/use-theme/use-theme.composable';
+import { Router } from '@/Router';
 
 export default function App() {
   const { currentTheme } = useTheme();
-  
+
   return (
-    <MantineProvider theme={currentTheme} defaultColorScheme='auto'>
+    <MantineProvider theme={currentTheme} defaultColorScheme="auto">
       <Router />
     </MantineProvider>
   );
